fb51351fe658169849e7de4759eb04e95095c3fc
Akiriwe <nata1016587@gmail.com>
2020-06-03 23:36:26 +0300

    Initial commit

---
+++ src/main/java/com/epam/spring/core/App.java
@@ -0,0 +1,13 @@
+package com.epam.spring.core;
+
+import com.epam.spring.core.beans.Client;
+import com.epam.spring.core.event.ConsoleEventLogger;
+
+public class App {
+    private ConsoleEventLogger consoleEventLogger;
+    private Client client;
+
+    public static void main(String[] args) {
+        
+    }
+}
